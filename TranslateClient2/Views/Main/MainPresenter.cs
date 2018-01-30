809cfa9374d5fe45204c14a2874664ea8bc919fe
Anton <anton_iv_al@mail.ru>
2018-01-31 02:54:18 +0300

    start

---
+++ TranslateClient2/Views/Main/MainPresenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TranslateClient2.Views.Main {
+    public class MainPresenter : Presenter<MainWindow> {
+        public MainPresenter(MainWindow view) : base(view) { }
+
+        public void OnLoaded() {
+//            _view.WindowState = WindowState.Minimized;
+            _view.Visibility = Visibility.Collapsed;
+        }
+
+        public void InputChanged(string text) {
+            _view.TranslatedText = text;
+        }
+    }
+}
---
+++ TranslateClient2/Views/Main/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TranslateClient2.Views.Main {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private readonly MainPresenter _presenter;
+
+        public MainWindow() {
+            InitializeComponent();
+            _presenter = new MainPresenter(this);
+        }
+
+        public string TranslatedText {
+            get => TranslatedControl.Content as string;
+            set => TranslatedControl.Content = value;
+        }
+
+        public string InputText {
+            get => InputControl.Text;
+            set => InputControl.Text = value;
+        }
+
+        private void InputControl_TextChanged(object sender, TextChangedEventArgs e) {
+            _presenter.InputChanged((sender as TextBox).Text);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            _presenter.OnLoaded();
+        }
+    }
+}
